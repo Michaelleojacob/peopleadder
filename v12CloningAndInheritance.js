db79392b116c760f6c5d348e6ddef5e6251e660b
Michael <michaelleojacob@gmail.com>
2021-10-09 12:55:50 -0700

    v12 cloning and inheritance makes sense and I added comments/notes.

---
+++ v12CloningAndInheritance.js
@@ -0,0 +1,37 @@
+//the purpose of this is to clone, and replace a method on the old object
+//objToClone will be getting cloned, and it's method moduleMethod is repalced.
+
+objToClone = {
+	var1: 'var1',
+	method1() {
+		return console.log('method1 was called');
+	},
+	var2: 'var2',
+	moduleMethod() {
+		return console.log('this is the old moduleMethod');
+	},
+};
+
+var test1 = (function (old) {
+	// console.log(old); //* (old === objToClone)
+	let my = {};
+
+	//* loop over 'old' obj, cloning it to 'my' obj
+	for (let key in old) {
+		if (old.hasOwnProperty(key)) {
+			my[key] = old[key];
+		}
+	}
+
+	var super_moduleMethod = old.moduleMethod;
+	// console.log(super_moduleMethod);
+	my.moduleMethod = function () {
+		//* override method on the clone, access to super through super_moduleMethod
+		return console.log('replaced lelelelelel');
+	};
+
+	return my;
+})(objToClone);
+
+objToClone.moduleMethod();
+test1.moduleMethod();
